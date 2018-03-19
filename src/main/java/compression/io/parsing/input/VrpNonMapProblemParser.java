d7c1b3c2ea59d1daded9e5f0625f5e472f2250ec
Mateusz <bakala12@o2.pl>
2018-03-19 19:25:34 +0100

    Starting development of VRP file parser

--- src/main/java/compression/io/parsing/input/VrpNonMapProblemParser.java
+++ src/main/java/compression/io/parsing/input/VrpNonMapProblemParser.java
@@ -1,12 +1,80 @@
 package compression.io.parsing.input;
 
+import com.graphhopper.reader.dem.SRTMProvider;
+import compression.io.parsing.ParsingException;
 import compression.model.vrp.VrpNonMapProblem;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class VrpNonMapProblemParser implements IVrpNonMapProblemParser{
+
     @Override
     public VrpNonMapProblem parse(InputStream stream) {
+        try(InputStreamReader reader = new InputStreamReader(stream)) {
+            try(BufferedReader bufferedReader = new BufferedReader(reader)){
+                return parseReader(bufferedReader);
+            }
+        } catch (IOException e) {
+            throw new ParsingException(e.getMessage(), e);
+        }
+    }
+
+    private VrpNonMapProblem parseReader(BufferedReader reader) throws IOException {
+        String line;
+        String name = null;
+        Double bestSolution = 0.0;
+        while((line = reader.readLine()) != null){
+            ParseResult<String> nameRes = parseName(line);
+            ParseResult<Double> optRes = parseOptimalSolution(line);
+            if(nameRes.hasValue){
+                name = nameRes.data;
+            }
+            if(optRes.hasValue){
+                bestSolution = optRes.data;
+            }
+
+        }
         return null;
     }
+
+    private class ParseResult<TData>{
+        @Getter
+        private TData data;
+        @Getter
+        private boolean hasValue;
+
+        public ParseResult(){
+            hasValue = false;
+        }
+
+        public ParseResult(TData data){
+            hasValue = true;
+            this.data = data;
+        }
+    }
+
+    private ParseResult<String> parseName(String line){
+        if(line.startsWith("NAME")){
+            String name = line.split(":")[1];
+            return new ParseResult(name);
+        }
+        return new ParseResult();
+    }
+
+    private ParseResult<Double> parseOptimalSolution(String line){
+        if(line.startsWith("COMMENT")){
+            String[] split = line.split(",");
+            String optimalStr = split[2].replace(")", "");
+            String bestSol = optimalStr.split(":")[1];
+            return new ParseResult<Double>(Double.parseDouble(bestSol));
+        }
+        return new ParseResult<>();
+    }
+
+
 }
--- src/main/java/compression/model/vrp/VrpNonMapProblem.java
+++ src/main/java/compression/model/vrp/VrpNonMapProblem.java
@@ -1,7 +1,12 @@
 package compression.model.vrp;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
 public class VrpNonMapProblem {
+    @Getter
+    private String name;
+    @Getter
+    private double bestKnownSolution;
 }
