48defcfc1cf9a99168d1d8ef087d83df472892e0
Mateusz <bakala12@o2.pl>
2018-04-03 08:23:55 +0200

    Major problem with jsprit shipment

--- src/main/java/compression/services/jsprit/JSpritService.java
+++ src/main/java/compression/services/jsprit/JSpritService.java
@@ -2,7 +2,9 @@ package compression.services.jsprit;
 
 import com.graphhopper.jsprit.core.algorithm.VehicleRoutingAlgorithm;
 import com.graphhopper.jsprit.core.algorithm.box.Jsprit;
+import com.graphhopper.jsprit.core.algorithm.state.StateManager;
 import com.graphhopper.jsprit.core.problem.VehicleRoutingProblem;
+import com.graphhopper.jsprit.core.problem.constraint.ConstraintManager;
 import com.graphhopper.jsprit.core.problem.solution.VehicleRoutingProblemSolution;
 import compression.model.jsprit.VrpProblemSolution;
 import compression.model.vrp.VrpProblem;
@@ -10,6 +12,8 @@ import compression.services.jsprit.conversion.EuclideanMetricVrpProblemToJSpritC
 import compression.services.jsprit.conversion.ExplicitMetricVrpProblemToJSpritConverter;
 import compression.services.jsprit.conversion.IVrpProblemToJSpritConverter;
 import compression.services.jsprit.conversion.ProblemConversionException;
+import compression.services.jsprit.extensions.MyShipmentStateUpdater;
+
 import java.util.Collection;
 
 public class JSpritService implements IJSpritService {
@@ -29,23 +33,26 @@ public class JSpritService implements IJSpritService {
         }
     }
 
-    private VrpProblemSolution solveJSpritProblem(VehicleRoutingProblem jSpritProblem){
-        VehicleRoutingAlgorithm algorithm = Jsprit.createAlgorithm(jSpritProblem);
-        Collection<VehicleRoutingProblemSolution> solutions = algorithm.searchSolutions();
-        return new VrpProblemSolution(jSpritProblem, solutions);
-    }
-
     @Override
     public VrpProblemSolution solve(VrpProblem problem){
         IVrpProblemToJSpritConverter converter = JSpritConvertersFactory.getConverter(problem);
         VehicleRoutingProblem vrp = converter.convertToJsprit(problem);
-        return solveJSpritProblem(vrp);
+        VehicleRoutingAlgorithm algorithm = Jsprit.createAlgorithm(vrp);
+        Collection<VehicleRoutingProblemSolution> solutions = algorithm.searchSolutions();
+        return new VrpProblemSolution(vrp, solutions);
     }
 
     @Override
     public VrpProblemSolution compressAndSolve(VrpProblem problem){
         IVrpProblemToJSpritConverter converter = JSpritConvertersFactory.getConverter(problem);
         VehicleRoutingProblem vrp = converter.compressAndConvertToJSprit(problem);
-        return solveJSpritProblem(vrp);
+        Jsprit.Builder algorithmBuilder = Jsprit.Builder.newInstance(vrp);
+        //StateManager stateManager = new StateManager(vrp);
+        //ConstraintManager constraintManager = new ConstraintManager(vrp, stateManager);
+        //stateManager.addStateUpdater(new MyShipmentStateUpdater(stateManager));
+        //algorithmBuilder.setStateAndConstraintManager(stateManager, constraintManager);
+        VehicleRoutingAlgorithm algorithm = algorithmBuilder.buildAlgorithm();
+        Collection<VehicleRoutingProblemSolution> solutions = algorithm.searchSolutions();
+        return new VrpProblemSolution(vrp, solutions);
     }
 }
