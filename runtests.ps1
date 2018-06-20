4c9822612f1d444d0911a8857525eff680cae392
Mateusz Bąkała <bakala12@o2.pl>
2018-06-20 11:37:21 +0200

    Improving script - error handling, always closing stream, flushing to results.csv file after single benchmark

--- runtests.ps1
+++ runtests.ps1
@@ -19,17 +19,31 @@ $resultFile = "results.csv"
 # Compression solution time
 # Time boost (*)
 
-$currDir = Split-Path -Path $MyInvocation.MyCommand.Path
+$stream = $null
 
-$stream = [System.IO.StreamWriter] "$currDir\$resultFile"
-[Void]$stream.WriteLine("BenchmarkName,Best known solution,Original size,Original solution vehicles,Original solution value,Original solution time,Compression size,Compression ratio,Compression solution,Compression solution quality loss,Compression time,Compression solution time,Time boost")
-Write-Output "Performing benchmark tests"
-foreach($benchmark in Get-ChildItem $inputDir){
-	$benchmarkName = $benchmark.BaseName
-	Write-Output $benchmarkName
-	.\runtest -BenchmarkName $benchmarkName -InputDir $inputDir -OutputDir $outputDir -DataDir $dataDir -PlotDir $plotDir
-	$res = .\collectResult -BenchmarkName $benchmarkName -ResultDir $outputDir
-	[Void]$stream.WriteLine($res)
+Try{
+	$currDir = Split-Path -Path $MyInvocation.MyCommand.Path
+
+	$stream = [System.IO.StreamWriter] "$currDir\$resultFile"
+	[Void]$stream.WriteLine("BenchmarkName,Best known solution,Original size,Original solution vehicles,Original solution value,Original solution time,Compression size,Compression ratio,Compression solution,Compression solution quality loss,Compression time,Compression solution time,Time boost")
+	Write-Output "Performing benchmark tests"
+	foreach($benchmark in Get-ChildItem $inputDir){
+		$benchmarkName = $benchmark.BaseName
+		Write-Output $benchmarkName
+		.\runtest -BenchmarkName $benchmarkName -InputDir $inputDir -OutputDir $outputDir -DataDir $dataDir -PlotDir $plotDir
+		$res = .\collectResult -BenchmarkName $benchmarkName -ResultDir $outputDir
+		[Void]$stream.WriteLine($res)
+		[Void]$stream.Flush()
+	}
+	Write-Output "Tests finished"
 }
-Write-Output "Tests finished"
-[Void]$stream.close()
+Catch
+{
+	$errorMessage = $_.Exception.Message
+	Write-Output "Exception: $errorMessage"
+}
+Finally{
+	if($stream -ne $null){
+		[Void]$stream.close()
+	}
+}
