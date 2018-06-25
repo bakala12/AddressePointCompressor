1d79027cf5abe12d7ae49bbb7f051952ccd2a339
Mateusz Bąkała <maba@netcompany.com>
2018-06-25 12:59:00 +0200

    Adding writing solution route to file

--- runtest.ps1
+++ runtest.ps1
@@ -23,6 +23,8 @@ $plotDirNormal = "$PlotDir\$BenchmarkName\$($BenchmarkName)Normal"
 $plotDirCompression = "$PlotDir\$BenchmarkName\$($BenchmarkName)Compression"
 $resultFileNameNormal = "$OutputDir\$BenchmarkName\$($BenchmarkName)Full.result"
 $resultFileNameCompression = "$OutputDir\$BenchmarkName\$($BenchmarkName)Compressed.result"
+$solutionRouteFileFull = "$OutputDir\$BenchmarkName\$($BenchmarkName)Full.route"
+$solutionRouteFileCompressed = "$OutputDir\$BenchmarkName\$($BenchmarkName)Compressed.route"
 
 New-Item -Force -Name "$OutputDir" -ItemType Directory | Out-Null
 New-Item -Force -Name "$OutputDir\$BenchmarkName" -ItemType Directory | Out-Null
@@ -36,6 +38,6 @@ New-Item -Force -Name "$plotDirCompression" -ItemType Directory | Out-Null
 $currDir = Split-Path -Path $MyInvocation.MyCommand.Path
 
 Write-Output "Start test for $($BenchmarkName)"
-Start-Process java -ArgumentList "-jar $pathToJar -i $currDir\$inputFile -o $currDir\$outputFile -d $currDir\$dataFile -p $currDir\$plotDirNormal -iter $Iterations -r $currDir\$resultFileNameNormal" -Wait -NoNewWindow
-Start-Process java -ArgumentList "-jar $pathToJar -i $currDir\$inputFile -o $currDir\$outputFileCompression -d $currDir\$dataFileCompression -p $currDir\$plotDirCompression -iter $Iterations -c -r $currDir\$resultFileNameCompression" -Wait -NoNewWindow
+Start-Process java -ArgumentList "-jar $pathToJar -i $currDir\$inputFile -o $currDir\$outputFile -d $currDir\$dataFile -p $currDir\$plotDirNormal -iter $Iterations -r $currDir\$resultFileNameNormal -s $currDir\$solutionRouteFileFull" -Wait -NoNewWindow
+Start-Process java -ArgumentList "-jar $pathToJar -i $currDir\$inputFile -o $currDir\$outputFileCompression -d $currDir\$dataFileCompression -p $currDir\$plotDirCompression -iter $Iterations -c -r $currDir\$resultFileNameCompression -s $currDir\$solutionRouteFileCompressed" -Wait -NoNewWindow
 Write-Output "End test for $($BenchmarkName)"
